21bf3b174f58a6dbae5b8c9bf54248d0d1d9c79e
IAMDEH <ee_izri@esi.dz>
2020-03-05 21:35:50 +0000

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ const port = 8080;
 
 console.log(process.env.ENVIRONMENT || 'e2e');
 
-if (process.env.ENVIRONMENT === 'prod') {	
+console.log(process.env.ENVIRONMENT || 'prod');
+
+/*if (process.env.ENVIRONMENT === 'prod') {	
     process.exit(1);	
-}
+}*/
 
 const server = http.createServer((_, res) => {
   res.statusCode = 200;
